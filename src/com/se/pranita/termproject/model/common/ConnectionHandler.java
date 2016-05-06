6ecf4b44cf9b1f7a06ba922a2e253acd478311be
Pranita Setiya <pranitasethiya@gmail.com>
2016-05-05 18:22:02 -0700

    fully working

--- src/com/se/pranita/termproject/model/common/ConnectionHandler.java
+++ src/com/se/pranita/termproject/model/common/ConnectionHandler.java
@@ -27,37 +27,37 @@ public class ConnectionHandler {
     }
 
     //local
-//    public static Connection getConnection() {
-//        ConnectionHandler cHandler = new ConnectionHandler();
-//        cHandler.DbConnection();
-//        String connectionString = "jdbc:mysql://" + address + "/" + dbName;
-//
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver").newInstance();
-//            connection = DriverManager.getConnection(connectionString, username, password);
-//            connection.setAutoCommit(false);
-//        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return connection;
-//    }
-
-    //deploy
     public static Connection getConnection() {
-        try {
-            URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+        ConnectionHandler cHandler = new ConnectionHandler();
+        cHandler.DbConnection();
+        String connectionString = "jdbc:mysql://" + address + "/" + dbName;
 
-            String username = jdbUri.getUserInfo().split(":")[0];
-            String password = jdbUri.getUserInfo().split(":")[1];
-            String port = String.valueOf(jdbUri.getPort());
-            String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-            connection = DriverManager.getConnection(jdbUrl, username, password);
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection(connectionString, username, password);
             connection.setAutoCommit(false);
-        } catch (URISyntaxException | SQLException e) {
+        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         return connection;
     }
+
+    //deploy
+//    public static Connection getConnection() {
+//        try {
+//            URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+//
+//            String username = jdbUri.getUserInfo().split(":")[0];
+//            String password = jdbUri.getUserInfo().split(":")[1];
+//            String port = String.valueOf(jdbUri.getPort());
+//            String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+//            connection = DriverManager.getConnection(jdbUrl, username, password);
+//            connection.setAutoCommit(false);
+//        } catch (URISyntaxException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return connection;
+//    }
 
 }
